2e058e779e9d890cf17717e1d2f6c346f2eb7062
Flaconi-Nicolas-Wyrwa <Nicolas.Wyrwa@flaconi.de>
2020-08-17 16:56:49 +0200

    bumping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bi_utils',
-    version='0.0.8',
+    version='0.0.9',
     description='common utility library shared between DE and DS teams to avoid duplication and maintenance efforts',
     url='http://github.com/Flaconi/bi_utils.git', 
     author='Ben Bauer & Anna Anisienia',
